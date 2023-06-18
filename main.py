9d9eeb4b5e421a9e537ea30487cbb56c19d01011
kawamura800 <mamedaihuku800@gmail.com>
2023-06-18 11:31:14 +0000

    🐛 little

--- main.py
+++ main.py
@@ -34,11 +34,6 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-def Hello():
-    return {"Hello": "World!"}
-
-
 # @app.post("/users/", response_model=schemas.User)
 # def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 #     db_user = crud.get_user_by_email(db, email=user.email)
